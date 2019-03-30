b1eeec6dd0dc761bbc5c44e16b523974b060aecb
CameronFranco <44078559+CameronFranco@users.noreply.github.com>
2019-03-29 23:50:14 -0400

    2

---
+++ GameUI/Assets/UIScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScript : MonoBehaviour
+{
+    public Text HealthText;
+    int Health = 10;
+    int width;
+    int height;
+
+    public void SetWidth(int newW)
+    {
+        width = newW;
+    }
+
+    public void SetHeight(int newH)
+    {
+        height = newH;
+    }
+
+    public void SetRes()
+    {
+        Screen.SetResolution(width,height,false);
+        Debug.Log( "Res changed to " + width + "x" + height);
+    }
+    void Update()
+
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Health--;
+        }
+
+        HealthText.text = "Health:" + Health;
+    }
+
+
+}
