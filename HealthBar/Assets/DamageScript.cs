6d989f99c05f6bbd54c54b12b5446cb68811fdbf
CameronFranco <44078559+CameronFranco@users.noreply.github.com>
2019-03-30 23:22:51 -0400

    3

---
+++ HealthBar/Assets/DamageScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        PlayerScript.health -= 10f;
+    }
+}
---
+++ HealthBar/Assets/PlayerScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour
+{
+     Image healthBar;
+    float maxHealth = 100f;
+    public static float health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthBar = GetComponent<Image> ();
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthBar.fillAmount = health/maxHealth;
+        if (health <= 100f)
+        Debug.Log("You have died!!!!!!!");
+    }
+}
